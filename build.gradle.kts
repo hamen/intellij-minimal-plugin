b97b6a0ddc39a4e6cce960fe409885547bf145a3
Ivan Morgillo <imorgillo@gmail.com>
2024-08-22 18:58:12 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,108 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+  id("java") // Java support
+  alias(libs.plugins.kotlin) // Kotlin support
+  alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin
+}
+
+group = providers.gradleProperty("pluginGroup").get()
+
+version = providers.gradleProperty("pluginVersion").get()
+
+// Set the JVM language level used to build the project.
+kotlin { jvmToolchain(17) }
+
+// Configure project's dependencies
+repositories {
+  mavenCentral()
+
+  // IntelliJ Platform Gradle Plugin Repositories Extension - read more:
+  // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
+  intellijPlatform { defaultRepositories() }
+}
+
+// Dependencies are managed with Gradle version catalog - read more:
+// https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
+dependencies {
+  testImplementation(libs.junit)
+
+  // IntelliJ Platform Gradle Plugin Dependencies Extension - read more:
+  // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+  intellijPlatform {
+    create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+
+    // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file
+    // for bundled IntelliJ Platform plugins.
+    bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for
+    // plugin from JetBrains Marketplace.
+    plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+
+    instrumentationTools()
+    pluginVerifier()
+    zipSigner()
+    testFramework(TestFrameworkType.Platform)
+  }
+}
+
+// Configure IntelliJ Platform Gradle Plugin - read more:
+// https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
+intellijPlatform {
+  pluginConfiguration {
+    version = providers.gradleProperty("pluginVersion")
+
+    ideaVersion {
+      sinceBuild = providers.gradleProperty("pluginSinceBuild")
+      untilBuild = providers.gradleProperty("pluginUntilBuild")
+    }
+  }
+  
+  buildSearchableOptions = false
+  autoReload = false
+
+  signing {
+    certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+    privateKey = providers.environmentVariable("PRIVATE_KEY")
+    password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+  }
+
+  publishing {
+    token = providers.environmentVariable("PUBLISH_TOKEN")
+    // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release
+    // labels, like 2.1.7-alpha.3
+    // Specify pre-release label to publish the plugin in a custom Release Channel automatically.
+    // Read more:
+    // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+    channels =
+        providers.gradleProperty("pluginVersion").map {
+          listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" })
+        }
+  }
+
+  pluginVerification { ides { recommended() } }
+}
+
+tasks {
+  wrapper { gradleVersion = providers.gradleProperty("gradleVersion").get() }
+}
+
+intellijPlatformTesting {
+  runIde {
+    register("runIdeForUiTests") {
+      task {
+        jvmArgumentProviders += CommandLineArgumentProvider {
+          listOf(
+              "-Drobot-server.port=8082",
+              "-Dide.mac.message.dialogs.as.sheets=false",
+              "-Djb.privacy.policy.text=<!--999.999-->",
+              "-Djb.consents.confirmation.enabled=false",
+          )
+        }
+      }
+
+      plugins { robotServerPlugin() }
+    }
+  }
+}
---
+++ src/main/kotlin/org/jetbrains/plugins/template/toolWindow/MyToolWindowFactory.kt
@@ -0,0 +1,43 @@
+package org.jetbrains.plugins.template.toolWindow
+
+import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBPanel
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.content.ContentFactory
+import com.intellij.util.ui.components.BorderLayoutPanel
+import java.awt.Dimension
+import javax.swing.BoxLayout
+import org.jetbrains.plugins.template.services.MyProjectService
+
+class MyToolWindowFactory : ToolWindowFactory {
+
+    init {
+        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+    }
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val myToolWindow = MyToolWindow(toolWindow)
+        val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
+        toolWindow.contentManager.addContent(content)
+    }
+
+    override fun shouldBeAvailable(project: Project) = true
+
+    class MyToolWindow(toolWindow: ToolWindow) {
+
+        private val service = toolWindow.project.service<MyProjectService>()
+
+        fun getContent() = BorderLayoutPanel().apply {
+            val boxPanel = JBPanel<JBPanel<*>>().apply {
+                layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                val textField = JBTextField()
+                add(textField)
+            }
+            addToTop(boxPanel)
+        }
+    }
+}
