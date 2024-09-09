3599bac0f70e0f802d98bcbbb92e4bde693e53f9
Ivan Morgillo <imorgillo@gmail.com>
2024-09-09 15:42:13 +0200

    add ComboBoxes
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

--- src/main/kotlin/com/github/hamen/intellijminimalplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/hamen/intellijminimalplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,23 +1,15 @@
 package com.github.hamen.intellijminimalplugin.toolWindow
 
-import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.ui.components.JBPanel
-import com.intellij.ui.components.JBTextField
 import com.intellij.ui.content.ContentFactory
 import com.intellij.util.ui.components.BorderLayoutPanel
-import java.awt.Dimension
-import javax.swing.BoxLayout
-import com.github.hamen.intellijminimalplugin.services.MyProjectService
+import javax.swing.DefaultComboBoxModel
+import javax.swing.JPanel
 
 class MyToolWindowFactory : ToolWindowFactory {
-
-    init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val myToolWindow = MyToolWindow(toolWindow)
@@ -29,15 +21,28 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     class MyToolWindow(toolWindow: ToolWindow) {
 
-        private val service = toolWindow.project.service<MyProjectService>()
+        fun getContent() =
+            BorderLayoutPanel().apply {
+                val zoomLevels = arrayOf("100%", "125%", "150%", "175%", "200%", "300%")
+                val comboBox1 = ComboBox(DefaultComboBoxModel(zoomLevels))
 
-        fun getContent() = BorderLayoutPanel().apply {
-            val boxPanel = JBPanel<JBPanel<*>>().apply {
-                layout = BoxLayout(this, BoxLayout.Y_AXIS)
-                val textField = JBTextField()
-                add(textField)
-            }
-            addToTop(boxPanel)
+                val fontSizes = arrayOf("12", "14", "15", "20", "25", "40")
+                val comboBox2 = ComboBox(DefaultComboBoxModel(fontSizes))
+
+                val letters = arrayOf("A", "B", "C", "D", "E", "F")
+                val comboBox3 = ComboBox(DefaultComboBoxModel(letters))
+
+                val randomWords =
+                    arrayOf("Cat", "Elephant", "Sun", "Book", "Laughter", "Whisper", "Ocean", "Serendipity lorem ipsum", "Umbrella", "Joy")
+                val comboBox4 = ComboBox(DefaultComboBoxModel(randomWords))
+
+                val wrapperPanel = JPanel()
+                wrapperPanel.add(comboBox1)
+                wrapperPanel.add(comboBox2)
+                wrapperPanel.add(comboBox3)
+                wrapperPanel.add(comboBox4)
+
+                addToCenter(wrapperPanel)
         }
     }
 }
